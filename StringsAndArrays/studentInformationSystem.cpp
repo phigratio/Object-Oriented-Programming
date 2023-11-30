1d1bad557943661c2ee9cdd7ff967582bbd9d728
Muqtadir <muqtadir.mujahid02@gmail.com>
2023-11-30 17:39:06 +0600

    student information system

--- StringsAndArrays/studentInformationSystem.cpp
+++ StringsAndArrays/studentInformationSystem.cpp
@@ -1,50 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Student{
-    private:
+class Student
+{
+private:
     string name;
     string dateOfBirth;
     int id;
     float cgpa;
-    public:
-    Student():name(""),dateOfBirth(""),id(0),cgpa(0.0){}
 
-    void setName(const string& n){name=n;}
-    void setDateofBirth(const string& dob){dateOfBirth=dob;}
-    void setId(int studentId){id=studentId;}
-    void setCGPA(float studentCGPA){cgpa=studentCGPA;}
+public:
+    Student() : name(""), dateOfBirth(""), id(0), cgpa(0.0) {}
 
-    string getName() const{return name;}
-    string getDateOfBirth()const{return dateOfBirth;}
-    int getId()const{return id;}
-    float getCGPA()const {return cgpa;}
+    void setName(const string &n) { name = n; }
+    void setDateofBirth(const string &dob) { dateOfBirth = dob; }
+    void setId(int studentId) { id = studentId; }
+    void setCGPA(float studentCGPA) { cgpa = studentCGPA; }
 
-    void getInfo() const{
-        cout<<"Name: "<<getName()<<endl;
-        cout<<"Date of Birth: "<<getDateOfBirth()<<endl;
-        cout<<"ID: "<<getId()<<endl;
-        cout<<"CGPA: "<<getCGPA()<<endl;
+    string getName() const { return name; }
+    string getDateOfBirth() const { return dateOfBirth; }
+    int getId() const { return id; }
+    float getCGPA() const { return cgpa; }
+
+    void getInfo() const
+    {
+        cout << "Name: " << getName() << endl;
+        cout << "Date of Birth: " << getDateOfBirth() << endl;
+        cout << "ID: " << getId() << endl;
+        cout << "CGPA: " << getCGPA() << endl;
     }
 
-    int getAge(const string& currentDate)const{
-        int currentYear=stoi(currentDate.substr(0,4));
-        int birthYear=stoi(dateOfBirth.substr(0,4));
+    int getAge(const string &currentDate) const
+    {
+        int currentYear = stoi(currentDate.substr(0, 4));
+        int birthYear = stoi(dateOfBirth.substr(0, 4));
 
-        return currentYear-birthYear;
+        return currentYear - birthYear;
     }
 
-    bool isValidDate(const string& date)const{
-        if(date.length()!=10)
+    bool isValidDate(const string &date) const
+    {
+        if (date.length() != 10)
         {
             return false;
         }
 
-        try{
-            stoi(date.substr(0,4));
-            stoi(date.substr(5,2));
-            stoi(date.substr(8,2));
+        istringstream iss(date);
+        int year, month, day;
+        char delimiter1, delimiter2;
+        if (!(iss >> year >> delimiter1 >> month >> delimiter2 >> day) ||
+            delimiter1 != '-' || delimiter2 != '-')
+        {
+            return false;
         }
-        catch(const exception& e)
+        if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31)
         {
             return false;
         }
@@ -52,3 +60,28 @@ class Student{
         return true;
     }
 };
+
+int main()
+{
+    Student student;
+
+    student.setName("John Doe");
+    student.setDateofBirth("2000-01-15");
+    student.setId(12345);
+    student.setCGPA(3.8);
+
+    student.getInfo();
+
+    string currentDate = "2023-11-30";
+    if (student.isValidDate(currentDate))
+    {
+        int age = student.getAge(currentDate);
+        cout << "Age: " << age << " years" << endl;
+    }
+    else
+    {
+        cout << "Invalid date format." << endl;
+    }
+
+    return 0;
+}
